61ffc1ef79853de396f0b5f8eccba0fc7caad46a
Pedro Dias <pdr050203@gmail.com>
2024-03-01 19:25:16 -0300

    add: import alias

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Home } from './src/screens/Home';
+import { Home } from '@screens/Home/index';
 
 export default function App() {
   return (
