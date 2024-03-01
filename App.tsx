6a2d0347e3ca2c7b57537658bac399389c9c3dd6
Pedro Dias <pdr050203@gmail.com>
2024-03-01 20:54:25 -0300

    add: theme and font

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 import { Home } from '@screens/Home/index';
+import theme from '@theme/index';
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito';
+import { ActivityIndicator, View } from 'react-native';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts([ Nunito_400Regular, Nunito_700Bold])
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      {isFontsLoaded ? <Home /> : <ActivityIndicator /> }
+    </ThemeProvider>
   );
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,32 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+
+    GREEN_300: '#E5F0DB',
+    GREEN_700: '#639339',
+    GREEN_500: '#CBE4B4',
+
+    RED_300: '#F4E6E7',
+    RED_500: '#F3BABD',
+    RED_700: '#BF3B44',
+
+    GRAY_700: '#1B1D1E',
+    GRAY_600: '#333638',
+    GRAY_500: '#5C6265',
+    GRAY_400: '#B9BBBC',
+    GRAY_300: '#EFF0F0',
+    GRAY_200: '#FAFAFA',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Nunito_400Regular',
+    BOLD: 'Nunito_700Bold'
+  },
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    XXL: 32
+  }
+};
