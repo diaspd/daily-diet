f4cd7dbf5c58412e830e32db3a39f0c20c779947
Pedro Dias <pdr050203@gmail.com>
2024-03-01 21:07:47 -0300

    add: status bar

--- App.tsx
+++ App.tsx
@@ -4,12 +4,18 @@ import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/
 
 import { Loading } from '@components/Loading';
 import { Home } from '@screens/Home/index';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [isFontsLoaded] = useFonts([ Nunito_400Regular, Nunito_700Bold])
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle='dark-content'
+        backgroundColor="transparent"
+        translucent
+      />
       {!isFontsLoaded ? <Home /> : <Loading /> }
     </ThemeProvider>
   );
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export function Home() {
