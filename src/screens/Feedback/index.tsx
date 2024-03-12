acb304e4a50c872227e72cf7742499e248ced6f0
Pedro Dias <pdr050203@gmail.com>
2024-03-12 18:01:17 -0300

    add: feedback screen

--- src/screens/Feedback/index.tsx
+++ src/screens/Feedback/index.tsx
@@ -1,10 +1,36 @@
-import { Container} from './styles';
+import { Container, Illustration, Title, Description } from './styles';
+
+import SuccessImg from '@assets/success.png';
+import FailImg from '@assets/fail.png';
+
+import { Button } from '@components/Button';
+
+import { useNavigation } from "@react-navigation/native";
 
 export function Feedback() {
-  
+  const navigation = useNavigation()
+
+  function handleGoHome() {
+    navigation.navigate('home');
+  }
+
   return (
     <Container>
-    
+        
+      <Title>Continue assim!</Title>
+
+      <Description>
+        Você continua dentro da dieta. Muito bem!
+      </Description>
+
+
+      <Illustration source={SuccessImg}/>
+
+      <Button 
+        onPress={handleGoHome}
+        title='Ir para a página inicial' 
+      />
+      
     </Container>
   );
 }
--- src/screens/Feedback/styles.ts
+++ src/screens/Feedback/styles.ts
@@ -1,9 +1,35 @@
 import { SafeAreaView } from "react-native";
-import styled, { useTheme } from "styled-components/native";
+import styled, { css, useTheme } from "styled-components/native";
 
 export const Container = styled(SafeAreaView)`
   flex: 1;
+  justify-content: center;
+  align-items: center;
+
   background-color: ${() => useTheme().COLORS.GRAY_200};
   padding: 24px;
   padding-top: 60px;
 `;
+
+export const Illustration = styled.Image`
+  width: 224px;
+  height: 288px;
+  margin-bottom: 32px;
+`;
+
+export const Title = styled.Text`
+  ${() => css`
+    font-size: ${useTheme().FONT_SIZE.XL}px;
+    color: ${useTheme().COLORS.GREEN_700};
+    font-family: ${useTheme().FONT_FAMILY.BOLD};
+  `}
+`
+
+export const Description = styled.Text`
+  ${() => css`
+    font-size: ${useTheme().FONT_SIZE.LG}px;
+    color: ${useTheme().COLORS.GRAY_700};
+    font-family: ${useTheme().FONT_FAMILY.REGULAR};
+    margin: 12px 0 40px 0;
+  `}
+`
