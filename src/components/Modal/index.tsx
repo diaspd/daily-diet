a8dfcf863ef6424840fa948f3feb7775ad1ab3a6
Pedro Dias <pdr050203@gmail.com>
2024-04-15 21:49:17 -0300

    add: remove meal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,13 +2,22 @@ import { Button } from '@components/Button';
 import { Overlay, Title, Container, ButtonWrapper } from './styles';
 import { Modal as ReactNativeModal } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
+import { mealDelete } from '@storage/meal/mealDelete';
+import { Dispatch, SetStateAction } from 'react';
 
-export function Modal({isModalVisible, setModalVisible, id, date}: any) {
+type ModalProps = {
+  isModalVisible: boolean;
+  setModalVisible: Dispatch<SetStateAction<boolean>>;
+  date: number;
+  id: string;
+}
+
+export function Modal({isModalVisible, setModalVisible, id, date}: ModalProps) {
   const navigation = useNavigation()
   
   async function handleRemoveMeal(id: string, date: number) {
     try {
-      console.log('deleted')
+      await mealDelete(id, date);
       navigation.goBack();
     } catch (error) {
       console.log(error);
---
+++ src/storage/meal/mealDelete.ts
@@ -0,0 +1,32 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { MEAL_COLLECTION } from '@storage/storageConfig';
+import { formatDate } from '@utils/formatDate';
+import { mealsGetAll } from './mealsGetAll';
+
+export async function mealDelete(id: string, date: number) {
+  try {
+    const storageData = await mealsGetAll();
+    const dataByDate = storageData.find(
+      (item) => item.title === formatDate(date, 'date')
+    );
+
+    if (dataByDate) {
+      const filteredMeals = dataByDate.data.filter((meal) => meal.id !== id);
+      dataByDate.data = filteredMeals;
+      const updatedStorageDataWhenNoMeals = storageData.filter(
+        (item) => item.title !== dataByDate.title
+      );
+
+    await AsyncStorage.setItem(
+      MEAL_COLLECTION,
+      JSON.stringify(
+        filteredMeals.length === 0
+          ? updatedStorageDataWhenNoMeals
+          : storageData
+      )
+    );
+    }
+  } catch (error) {
+    throw error;
+  }
+}
